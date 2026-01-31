028813b34679e9481ec68c6788ac47bd0b8198df
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-01-31 19:40:52 -0300

    feat: add modal new

---
+++ interviewbackend/src/main/java/backend/models/InterviewStepModel.java
@@ -0,0 +1,17 @@
+package backend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class InterviewStepModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}

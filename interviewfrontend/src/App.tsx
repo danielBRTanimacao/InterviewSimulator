42701e82a38755b9cad2528d6665ca798c095dba
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-02-07 08:41:22 -0300

    testing shadcn

--- interviewfrontend/src/App.tsx
+++ interviewfrontend/src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export default () => {
-    return <h1>opa</h1>;
+    return (
+        <>
+            <h1>Aooooooba</h1>
+            <Button>Clica ai pa nois testar</Button>
+        </>
+    );
 };

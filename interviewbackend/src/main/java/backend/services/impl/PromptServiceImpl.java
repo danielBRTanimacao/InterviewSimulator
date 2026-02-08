546633c4a4e0bf7f76ce8f5f3173c242f02a1243
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-02-08 09:19:00 -0300

    feat: updating backend testing controll

--- interviewbackend/src/main/java/backend/services/impl/PromptServiceImpl.java
+++ interviewbackend/src/main/java/backend/services/impl/PromptServiceImpl.java
@@ -1,34 +1,21 @@
 package backend.services.impl;
 
 import backend.models.PromptModel;
-import backend.repositories.PromptRepository;
 import backend.services.PromptService;
 import backend.utils.QuestionGenerator;
-import com.google.genai.Client;
-import com.google.genai.types.GenerateContentResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PromptServiceImpl implements PromptService {
-    @Value("${spring.genai.key}")
-    private String apiKey;
 
     //private final PromptRepository repository;
+    private final QuestionGenerator generator;
 
     @Override
     public void savePrompt(PromptModel data) {
-        Client client = Client.builder().apiKey(apiKey).build();
-
-        GenerateContentResponse response =
-                client.models.generateContent(
-                        "gemini-3-flash-preview",
-                        QuestionGenerator.generateTemplate(data),
-                        null);
-
-        System.out.println(response.text());
+        generator.contentImplement(data);
 
         //repository.save(data);
     }
--- interviewbackend/src/main/java/backend/utils/QuestionGenerator.java
+++ interviewbackend/src/main/java/backend/utils/QuestionGenerator.java
@@ -1,10 +1,35 @@
 package backend.utils;
 
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
+
 import backend.models.PromptModel;
 import backend.models.enums.TypeInterview;
+import lombok.RequiredArgsConstructor;
 
+@Component
+@RequiredArgsConstructor
 public class QuestionGenerator {
-    public static String generateTemplate(PromptModel model) {
+    @Value("${spring.genai.key}")
+    private String apiKey;
+    
+    public void contentImplement(PromptModel data) {
+        Client client = Client.builder().apiKey(apiKey).build();
+
+        GenerateContentResponse response =
+                client.models.generateContent(
+                        "gemini-3-flash-preview",
+                        this.generateTemplate(data),
+                        null);
+
+        System.out.println(response.text());
+    }
+
+    public String generateTemplate(PromptModel model) {
         String contexto = (model.getType() == TypeInterview.TECHNICAL)
                 ? "Foque em algoritmos, arquitetura e sintaxe."
                 : "Foque em comportamento, resolução de conflitos e cultura.";

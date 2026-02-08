c7d3bab5be489717f57964e450d407b13a98bd24
danielBRTanimacao <danieltenorio2046@gmail.com>
2026-02-08 09:35:37 -0300

    feat: add tanstack router

--- interviewfrontend/src/main.tsx
+++ interviewfrontend/src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./assets/css/Index.css";
+import {
+    createRootRoute,
+    createRoute,
+    createRouter,
+    RouterProvider,
+} from "@tanstack/react-router";
+
 import App from "./App.tsx";
+import Index from "./assets/pages/interview/Index.tsx";
+
+const rootRoute = createRootRoute();
+
+const routes = [
+    createRoute({
+        getParentRoute: () => rootRoute,
+        path: "/",
+        component: App,
+    }),
+    createRoute({
+        getParentRoute: () => rootRoute,
+        path: "/interview",
+        component: Index,
+    }),
+];
+
+const routeTree = rootRoute.addChildren(routes);
+const router = createRouter({ routeTree });
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
+    <RouterProvider router={router} />,
 );
